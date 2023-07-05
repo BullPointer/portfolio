c9cb7b5e51f626df9d19618007acaf5c7ef2aa60
White Bull <blueded88@gmail.com>
2023-07-05 16:31:00 -0400

    adding changes to Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,15 +2,15 @@
 import { Icon } from "@iconify/react";
 const contact = () => {
   return (
-    <div className="bg-blue-700 gradient-bg-page py-20">
-      <div className="w-[60%] mx-auto">
-        <div>
+    <div id="contact" className="bg-blue-700 gradient-bg-page py-20">
+      <div className="w-[90%] md:w-[60%] mx-auto">
+        <div className="text-center">
           <div className="font-bold text-blue-700 pt-2">CONTACT</div>
           <div className="font-bold text-white text-2xl py-2">
             Don't be shy! Hit me up! 
           </div>
         </div>
-        <div className="grid grid-cols-2 py-8">
+        <div className="w-full grid justify-items-start grid-cols-1 md:grid-cols-2 py-8">
           <div className="flex flex-row justify-center items-center">
             <div className="px-4">
               <Icon className="text-yellow-800" icon="subway:call-1" fontSize={30} />
@@ -26,7 +26,7 @@ const contact = () => {
             </div>
             <div>
               <div className="text-gray-400 font-bold">Mail</div>
-              <div className="font-semibold text-white">
+              <div className="font-semibold text-white ">
                 blueded88@gmail.com
               </div>
             </div>
