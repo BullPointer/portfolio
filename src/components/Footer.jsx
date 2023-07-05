572e6d28de069727efb30a390e94a93885cd4e35
White Bull <blueded88@gmail.com>
2023-07-05 16:31:27 -0400

    adding changes to Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,22 +1,21 @@
-import { Icon } from '@iconify/react';
+import { Icon } from "@iconify/react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
     <div className="bg-blue-700 gradient-bg-page py-8">
-      {/* <ul className="flex w-full bg-pink-500 justify-center items-center gap-2">
-        {["Home", "About", "Projects", "Contact"].map((item, index) => (
-          <li key={index} className="text-white p-4 font-semibold">
-            {" "}
-            {item}{" "}
-          </li>
-        ))}
-      </ul> */}
-      <div className="sm:w-[90%] mx-auto w-full flex justify-between items-center pt-3 py-4">
+      <div className="sm:w-[90%] mx-auto w-full flex gap-5 md:gap-0 flex-col md:flex-row justify-between items-center md:pt-3 md:py-4">
         <p className="text-white text-sm text-center">@Youngponi 2023</p>
         <p className="flex flex-row gap-4 text-white">
-        <Icon icon="akar-icons:github-fill" fontSize={25} />
-        <Icon icon="devicon-plain:linkedin" fontSize={25} />
-        <Icon icon="cib:twitter" fontSize={25} />
+          <Link to={"https://github.com/BullPointer/"} target="_blank">
+            <Icon icon="akar-icons:github-fill" fontSize={25} />
+          </Link>
+          <Link to={"https://linkedin.com/in/youngminters/"} target="_blank">
+            <Icon icon="devicon-plain:linkedin" fontSize={25} />
+          </Link>
+          <Link to={"https://twitter.com/Jakewoft"} target="_blank">
+            <Icon icon="cib:twitter" fontSize={25} />
+          </Link>
         </p>
         <p className="text-white text-sm text-center">All rights reserved</p>
       </div>
