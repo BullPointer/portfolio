d1c4d37147c1bb68e0500706630fc905e08b00d4
White Bull <blueded88@gmail.com>
2023-08-10 03:17:32 -0400

    made some improvement and more to come

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,10 +1,7 @@
 /* eslint-disable react/prop-types */
 import { Icon } from "@iconify/react";
 import { Link } from "react-router-dom";
-import Img1 from "../assets/website1.png";
-import Img2 from "../assets/website2.png";
-import Img3 from "../assets/website3.png";
-import Img4 from "../assets/website4.png";
+import projectData from "./projectData";
 
 const ProjectItem = ({ img, title, note, code, demo }) => {
   return (
@@ -32,10 +29,7 @@ const ProjectItem = ({ img, title, note, code, demo }) => {
               />
             </div>
           </Link>
-          <Link
-            to={`https://bullpointer.github.io/${demo}`}
-            target="_blank"
-          >
+          <Link to={`https://bullpointer.github.io/${demo}`} target="_blank">
             <div className="flex flex-row gap-2 items-center justify-center cursor-pointer">
               <div className="text-red-400 font-extrabold">Live Demo</div>
               <Icon
@@ -64,42 +58,16 @@ const Projects = () => {
         </div>
       </div>
       <div className="flex flex-col">
-        <ProjectItem
-          img={Img1}
-          title={"Shopping Cart"}
-          note={
-            "Shopping Cart is an online Ecommerce that allows users to buy products"
-          }
-          code={"cart"}
-          demo={"cart"}
-        />
-        <ProjectItem
-          img={Img2}
-          title={"Resume Builder"}
-          note={
-            "Resume Builder is a web application that allows user build an resume"
-          }
-          code={"cv-builder"}
-          demo={"cv-builder"}
-        />
-        <ProjectItem
-          img={Img3}
-          title={"Swapmute"}
-          note={
-            "Swapmute is a crypto exchange application that allows users to exchange cryptocurrencies of their choice"
-          }
-          code={"swapmute"}
-          demo={"swapmute"}
-        />
-        <ProjectItem
-          img={Img4}
-          title={"Mayo Hospital"}
-          note={
-            "Mayo Hospital is mayoclinic.org's clone website I built, together with some added features in Blockchain technology"
-          }
-          code={"mayo-hospital"}
-          demo={"mayo-hospital"}
-        />
+        {projectData?.map(({ img, code, demo, note, title }, index) => (
+          <ProjectItem
+            key={index}
+            img={img}
+            title={title}
+            note={note}
+            code={code}
+            demo={demo}
+          />
+        ))}
       </div>
       <div></div>
     </div>
---
+++ src/components/projectData.js
@@ -0,0 +1,35 @@
+import Img1 from "../assets/website1.png";
+import Img2 from "../assets/website2.png";
+import Img3 from "../assets/website3.png";
+import Img4 from "../assets/website4.png";
+
+export default [
+    {
+        img: Img1,
+        title: "Shopping Cart",
+        note: "Shopping Cart is an online Ecommerce that allows users to buy products",
+        code: "cart",
+        demo: "cart",
+    },
+    {
+        img: Img2,
+        title: "Resume Builder",
+        note: "Resume Builder is a web application that allows user build an resume",
+        code: "cv-builder",
+        demo: "cv-builder",
+    },
+    {
+        img: Img3,
+        title: "Osicrypto",
+        note: "Osicrypto is a crypto exchange application that allows users to exchange cryptocurrencies of their choice",
+        code: "osicrypto",
+        demo: "osicrypto",
+    },
+    {
+        img: Img4,
+        title: "Mayo Hospital",
+        note: "Mayo Hospital is mayoclinic.org's clone website I built, together with some added features in Blockchain technology",
+        code: "mayo-hospital",
+        demo: "mayo-hospital",
+    }
+]
